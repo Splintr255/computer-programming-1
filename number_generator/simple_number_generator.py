f1adfece8f41f2cead74ab5aba147332980703da
Splintr255 <ericjohnhammond@gmail.com>
2021-01-28 12:32:13 -0700

    added simple_number_generator

---
+++ number_generator/simple_number_generator.py
@@ -0,0 +1,7 @@
+# variables
+import random
+num1 = int(input('Give me a Minimum: '))
+num2 = int(input('Give me a Maximum: '))
+
+# calculator
+print('Your number: ' + str(random.randrange(num1, num2)))
